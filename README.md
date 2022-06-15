803766857dd1ce9f2ef618221d3451591f41ea20
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-15 11:59:21 -0700

    Add and pass second test.
    Co-authored-by: Grace Kostanich <fl5812597@gmail.com>

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ die1 = new Die(6);
 rollValue = die1.roll();  
 Expected Output: rollValue; (_any # 1 - 6_) 
 
+Test: "It will add every dice roll's value to the players turn score and output the turn score total"  
+Code:  
+die = new Die(6);  
+player1 = new Player("Frank");  
+player1.turn(die);  
+player1.turn(die);  
+player1.turn(die);  
+console.log(player1.turnScore);     
+Expected Output: _turn#1 + turn#2 + turn#3 = total turnScore_
--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,29 @@ Die.prototype.roll = function(){
   return roll;
 };
 
-
-function(){
-  die = new die(6);
-  rollValue = die.roll();
+function Player(name){
+  this.name = name;
+  this.turnScore = 0;
+  this.totalScore = 0;
 }
 
+//this would happen when a player hits the roll button
+Player.prototype.turn = function(die){
+  let dieRoll = die.roll()
+  if(dieRoll != 1){
+    this.turnScore += dieRoll;
+  }else{
+    this.turnScore = 0;
+  }
+};
+
+//when a player holds, the turn score is added to the total score
+Player.prototype.hold = function(){
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
+};
+
+die = new Die(6);
+player1 = new Player("Frank");
+
+player1.turn(die);
