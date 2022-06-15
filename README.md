ff0b021728728897df6df6d8de9fe077407d9b95
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-15 15:43:07 -0700

    Add more functionality.
    Co-authored-by: Grace Kostanich <fl5812597@gmail.com>

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 Describe Die.prototype.roll():
 
-Test: "It will generate a random number based on the amount of sides of the die it's called on"  
+**Test:** "It will generate a random number based on the amount of sides of the die it's called on"  
 Code:  
 die1 = new Die(6);  
 rollValue = die1.roll();  
 Expected Output: rollValue; (_any # 1 - 6_) 
 
-Test: "It will add every dice roll's value to the players turn score and output the turn score total"  
+Describe Player.prototype.turn():
+
+**Test:** "It will add every dice roll's value to the players turn score and output the turn score total"  
 Code:  
 die = new Die(6);  
 player1 = new Player("Frank");  
@@ -15,3 +17,36 @@ player1.turn(die);
 player1.turn(die);  
 console.log(player1.turnScore);     
 Expected Output: _turn#1 + turn#2 + turn#3 = total turnScore_
+
+**Test:** "When a player scores 1, the turn score is set to zero"  
+Code:  
+die = new Die(6); 
+player1 = new Player("Frank");    
+player1.turn(die); _This roll will be 1_ 
+console.log(player1.turnScore);
+Expected Output: 0  
+
+**Test:** "When a players turn score is set to zero, switch to the other player"  
+Code:  
+die = new Die(6); 
+player1 = new Player("Frank");    
+player1.turn(die); _This roll will be 1_ 
+console.log(activePlayer);
+Expected Output: 
+
+
+Describe Player.prototype.hold():
+
+**Test:** "When a player holds, the turn score is added to the total score"  
+Code:  
+player1 = new Player("Frank");  
+player1.hold();  
+console.log(player1.totalScore);  
+Expected Output: _totalScore_  
+
+**Test:** "When a player holds, the turn score is reset to zero"  
+Code:  
+player1 = new Player("Frank");  
+player1.hold();  
+console.log(player1.turnScore);  
+Expected Output: 0 
--- js/scripts.js
+++ js/scripts.js
@@ -13,23 +13,86 @@ function Player(name){
   this.totalScore = 0;
 }
 
-//this would happen when a player hits the roll button
 Player.prototype.turn = function(die){
-  let dieRoll = die.roll()
+  let dieRoll = die.roll();
   if(dieRoll != 1){
     this.turnScore += dieRoll;
   }else{
     this.turnScore = 0;
+    this.hold();
   }
+  return dieRoll;
 };
 
-//when a player holds, the turn score is added to the total score
 Player.prototype.hold = function(){
   this.totalScore += this.turnScore;
   this.turnScore = 0;
+  toggleButtons();
 };
 
-die = new Die(6);
-player1 = new Player("Frank");
+//UI logic
 
-player1.turn(die);
+function toggleButtons(){
+  if ($('#player1Hold').prop('disabled', false)){
+    $('#player1Hold').prop('disabled', true);
+    $('#player1Turn').prop('disabled', true);
+    $('#player2Hold').prop('disabled', false);
+    $('#player2Turn').prop('disabled', false);
+  }else if ($('#player2Hold').prop('disabled', false)){
+    $('#player1Hold').prop('disabled', false);
+    $('#player1Turn').prop('disabled', false);
+    $('#player2Hold').prop('disabled', true);
+    $('#player2Turn').prop('disabled', true);
+  }
+}
+
+$("#player1Turn").click(function() {
+  player1.turn(die);
+  $("#rollValue").append(player1.turn(die))
+});
+$("#player1Hold").click(function() {
+  player1.hold();
+});
+
+$(document).ready(function() {
+  let player1 = new Player("");
+  let player2 = new Player("");
+  console.log(player1.name);
+  console.log(player2.name);
+  $("form#new-player-1").submit(function(event) {
+    event.preventDefault();
+    const player1Name = $("input#player-1-name").val();
+    player1.name = player1Name;
+  });
+  $("form#new-player-2").submit(function(event) {
+    event.preventDefault();
+    const player2Name = $("input#player-2-name").val();
+    player2.name = player2Name;
+  });
+  console.log(player1.name);
+  console.log(player2.name);
+});
+
+//Ex:|| Movie-Tickets
+// $('#howmuch-button').click(function(event) {
+//   event.preventDefault();
+//   const ticket = getTicket();
+//   // print in ticketReturn the price and the msg
+//   $('#ticket-price').text("$ " + ticket.priceMsg[0]);
+//   $('#ticket-price-msg').text(ticket.priceMsg[1]);
+//   $('.ticketReturn').toggle();
+//   // enable ticket buy button
+//   $('#buy-button').removeAttr('disabled');
+// });
+// $('form#ticketForm').submit(function(event) {
+//   event.preventDefault();
+//   const ticket = getTicket();
+//   ticketsSold.addTicket(ticket);
+//   attachSoldTickets(ticket);
+//   // clear fields and disable ticket buy button
+//   $('#buy-button').prop('disabled', true);
+//   clearTicketInformation();
+// });
+// });
+
+
